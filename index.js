9f1730ad6ed3df7563d98734661fe7c87659bd5c
Perry Jorss <pjorss123@gmail.com>
2022-04-06 16:33:44 -0400

    commit tree 57cff215ab2e3593f477fc3353a65e7013109c21

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    return Math.abs(block - 42)
+}
+
+function distanceFromHqInFeet(block){
+   let distanceInBlocks = distanceFromHqInBlocks(block)
+    return Math.abs(distanceInBlocks * 264)
+}
+
+function distanceTravelledInFeet(startBlock,endBlock){
+    let startAndEnd = startBlock - endBlock
+    return Math.abs(startAndEnd * 264)
+}
+
+function calculatesFarePrice(startBlock, endBlock){
+    let feetDifference = distanceTravelledInFeet(startBlock, endBlock)
+        if (feetDifference <= 400){
+            return 0
+        }
+        else if (feetDifference >400 && feetDifference <=2000){
+            return (feetDifference -400)* .02
+        }
+        else if (feetDifference >2000 && feetDifference <=2500){
+            return 25
+        }
+        else if (feetDifference >2500){
+            return 'cannot travel that far'
+        }
+}
